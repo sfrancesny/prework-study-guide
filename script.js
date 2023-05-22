4d420adf087156ce6670262192c54448037fa9c5
Sonia <Sonianyenkan80@gmail.com>
2023-05-22 13:40:42 -0400

    Debugged Code in Javascript function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+function listTopics() {
 for (var x = 0; x < topics.length; x++) {
  console.log(topics[x]);
+}
 }
 
 function selectTopic() {
@@ -18,12 +20,8 @@ if (randomTopic=== 'HTML') {
  console.log('Please try again!');
 }
 } 
-function listTopics() {
-  for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-  }
-}
+
 console.log('Here are the topics we learned through Prework:');
 listTopics();
-console.log('Here are the topics we learned through Prework:');
-listTopics();
+console.log('Which topic should we study first');
+selectTopic();
